51b9b27ecc0e650cfdc1addc0b03cbb87b90db40
anshika13raj <154364972+anshika13raj@users.noreply.github.com>
2026-06-28 13:43:39 +0530

    Update Dashboard.jsx

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,27 +1,50 @@
-/**
- * Dashboard Page
- */
+// /**
+//  * Dashboard Page
+//  */
+
+// import Header from "../components/Header";
+// import Analytics from "../components/Analytics";
+// import TransactionList from "../components/TransactionList";
+
+// function Dashboard() {
+
+//   return (
+
+//     <div className="container">
+
+//       <Header />
+
+//       <Analytics />
+
+//       <TransactionList />
+
+//     </div>
+
+//   );
+
+// }
+
+// export default Dashboard;
+
 
 import Header from "../components/Header";
 import Analytics from "../components/Analytics";
 import TransactionList from "../components/TransactionList";
 
 function Dashboard() {
-
   return (
-
     <div className="container">
-
       <Header />
+
+      <div style={{ background: "yellow", padding: "20px", margin: "20px 0" }}>
+        Dashboard is rendering
+      </div>
 
       <Analytics />
 
       <TransactionList />
-
     </div>
-
   );
-
 }
 
 export default Dashboard;
