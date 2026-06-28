bbb055e7da097101894a598a9ea7d8f5e829f32b
anshika13raj <154364972+anshika13raj@users.noreply.github.com>
2026-06-28 13:47:19 +0530

    Refactor Dashboard component and clean up code
    Removed commented-out code and added a rendering message.

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,50 +1,29 @@
-// /**
-//  * Dashboard Page
-//  */
-
-// import Header from "../components/Header";
-// import Analytics from "../components/Analytics";
-// import TransactionList from "../components/TransactionList";
-
-// function Dashboard() {
-
-//   return (
-
-//     <div className="container">
-
-//       <Header />
-
-//       <Analytics />
-
-//       <TransactionList />
-
-//     </div>
-
-//   );
-
-// }
-
-// export default Dashboard;
-
+/**
+ * Dashboard Page
+ */
 
 import Header from "../components/Header";
 import Analytics from "../components/Analytics";
 import TransactionList from "../components/TransactionList";
 
 function Dashboard() {
-  return (
-    <div className="container">
-      <Header />
 
-      <div style={{ background: "yellow", padding: "20px", margin: "20px 0" }}>
-        Dashboard is rendering
-      </div>
+  return (
+
+    <div className="container">
+
+      <Header />
 
       <Analytics />
 
       <TransactionList />
+
     </div>
+
   );
+
 }
 
 export default Dashboard;
+
+
