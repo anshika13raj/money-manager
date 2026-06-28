bee6ed91650f0db25056473331879439a12414b6
anshika13raj <154364972+anshika13raj@users.noreply.github.com>
2026-06-28 13:44:41 +0530

    Refactor TransactionList to display a static message
    Replaced the original TransactionList component with a simple div displaying a message.

--- client/src/components/TransactionList.jsx
+++ client/src/components/TransactionList.jsx
@@ -1,59 +1,76 @@
-/**
- * Displays all transactions
- */
+// /**
+//  * Displays all transactions
+//  */
 
-import { useTransactions } from "../context/TransactionContext";
+// import { useTransactions } from "../context/TransactionContext";
 
-import TransactionCard from "./TransactionCard";
+// import TransactionCard from "./TransactionCard";
+
+// function TransactionList() {
+
+//   const {
+
+//     transactions,
+
+//     loading
+
+//   } = useTransactions();
+
+//   if (loading)
+
+//     return <h2>Loading...</h2>;
+
+//   return (
+
+//     <section className="transaction-section">
+
+//       <h2>
+
+//         Recent Transactions
+
+//       </h2>
+
+//       {
+
+//         transactions.map(
+
+//           transaction => (
+
+//             <TransactionCard
+
+//               key={transaction.id}
+
+//               transaction={transaction}
+
+//             />
+
+//           )
+
+//         )
+
+//       }
+
+//     </section>
+
+//   );
+
+// }
+
+// export default TransactionList;
 
 function TransactionList() {
-
-  const {
-
-    transactions,
-
-    loading
-
-  } = useTransactions();
-
-  if (loading)
-
-    return <h2>Loading...</h2>;
-
   return (
-
-    <section className="transaction-section">
-
-      <h2>
-
-        Recent Transactions
-
-      </h2>
-
-      {
-
-        transactions.map(
-
-          transaction => (
-
-            <TransactionCard
-
-              key={transaction.id}
-
-              transaction={transaction}
-
-            />
-
-          )
-
-        )
-
-      }
-
-    </section>
-
+    <div
+      style={{
+        background: "green",
+        color: "white",
+        padding: "20px"
+      }}
+    >
+      Transaction List Loaded
+    </div>
   );
-
 }
 
 export default TransactionList;
+
